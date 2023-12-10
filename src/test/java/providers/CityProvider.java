a5172588021e240738d81247ddd259b944829827
tooniez <tooni22@proton.me>
2023-12-10 14:49:17 +1030

    feat: :building_construction: update CityProvider to return list of states

--- src/test/java/providers/CityProvider.java
+++ src/test/java/providers/CityProvider.java
@@ -1,4 +1,10 @@
 package providers;
 
+import java.util.Arrays;
+import java.util.List;
 
+public class CityProvider {
+    public static List<String> getCities() {
+        return Arrays.asList("Melbourne", "Manchester");
+    }
 }
