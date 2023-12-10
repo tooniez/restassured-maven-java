19e1ac0d283a3a382822e489b34bd4c7f07073c3
tooniez <tooni22@proton.me>
2023-12-10 16:12:30 +1030

    feat: :construction: load cities into test

--- src/test/java/TestLatLon.java
+++ src/test/java/TestLatLon.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+import providers.CityProvider;
+import java.util.List;
 public class TestLatLon extends BaseTest{
+
+    @Test
+    public void testLatLon() {
+        // Get weather data for Melbourne and Manchester from CityProvider
+        List<String> cities = CityProvider.getCities();
+
+    }
 }
