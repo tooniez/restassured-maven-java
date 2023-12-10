a24780020b1d20ce54a9532710c7b61a84dd4979
tooniez <tooni22@proton.me>
2023-12-10 17:54:21 +1030

    chore: :construction: update test to get response for each city

--- src/test/java/TestAirQuality.java
+++ src/test/java/TestAirQuality.java
@@ -1,6 +1,19 @@
+import providers.CityProvider;
+import weather.Endpoints;
 public class TestAirQuality extends BaseTest {
     @Test
     public void testAirQuality() {
 
+        List<String> cities = CityProvider.getCities();
+
+        for (String city : cities) {
+            // Get weather data for cities
+            Response weatherResponse = RestAssured.given()
+                    .when()
+                    .get(Endpoints.getBaseUrl() + Endpoints.getWeatherEndpoint(city, API_KEY));
+
+            System.out.println(weatherResponse.getBody().asString());
+            // Extract lat and lon for city
+        }
     }
 }
